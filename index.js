eafe1e0e9a5e5a677647d525a7b9fd16261bf794
jwong65 <jason.wong6507@gmail.com>
2022-12-13 20:47:14 -0500

    Comment out the color section so we could test the app.use

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ require(`dotenv`).config()
 function returnCapitalized(string){
     return string.charAt(0).toUpperCase() +string.slice(1)
 }
-app.get('/', (req, res)=>{
-    res.send (`
-    <body style="margin:0;">
-        <div style ="border: 1px solid black; height: 10vh; background-color: white">
-        <h2 style="text-align: center;"> Hello webpage</h2>
-        </div>
-        <h1>White page?</h1>
-        </body>
-    `)
-})
+// app.get('/', (req, res)=>{
+//     res.send (`
+//     <body style="margin:0;">
+//         <div style ="border: 1px solid black; height: 10vh; background-color: white">
+//         <h2 style="text-align: center;"> Hello webpage</h2>
+//         </div>
+//         <h1>White page?</h1>
+//         </body>
+//     `)
+// })
 
-app.get('/:color', (req, res)=>{
-    let myColor = req.params.color
-    let colorName = returnCapitalized(myColor)
-    res.send (`
-    <body style="margin:0;">
-        <div style ="border: 1px solid black; height: 10vh; background-color: ${myColor};">
-        <h2 style="text-align: center;"> Hello webpage</h2>
-        </div>
-        <h1 style="color:${myColor}">${colorName} page</h1>
-        </body>
-    `)
-})
+// app.get('/:color', (req, res)=>{
+//     let myColor = req.params.color
+//     let colorName = returnCapitalized(myColor)
+//     res.send (`
+//     <body style="margin:0;">
+//         <div style ="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+//         <h2 style="text-align: center;"> Hello webpage</h2>
+//         </div>
+//         <h1 style="color:${myColor}">${colorName} page</h1>
+//         </body>
+//     `)
+// })
 
 app.use('/more', require('./controllers/more'))
 
