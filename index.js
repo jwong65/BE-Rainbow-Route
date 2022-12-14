95d6d8c570ce139c4b8b8e777b9bd1971f603731
jwong65 <jason.wong6507@gmail.com>
2022-12-13 20:37:31 -0500

    Adding files

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+require(`dotenv`).config()
+
+function returnCapitalized(string){
+    return string.charAt(0).toUpperCase() +string.slice(1)
+}
+app.get('/', (req, res)=>{
+    res.send (`
+    <body style="margin:0;">
+        <div style ="border: 1px solid black; height: 10vh; background-color: white">
+        <h2 style="text-align: center;"> Hello webpage</h2>
+        </div>
+        <h1>White page?</h1>
+        </body>
+    `)
+})
+
+app.get('/:color', (req, res)=>{
+    let myColor = req.params.color
+    let colorName = returnCapitalized(myColor)
+    res.send (`
+    <body style="margin:0;">
+        <div style ="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+        <h2 style="text-align: center;"> Hello webpage</h2>
+        </div>
+        <h1 style="color:${myColor}">${colorName} page</h1>
+        </body>
+    `)
+})
+
+// app.get('/crimson', (req, res)=>{
+//     res.send (`
+//     <body style="margin:0;">
+//         <div style ="border: 1px solid black; height: 10vh; background-color: crimson;">
+//         <h2 style="text-align: center;"> Hello webpage</h2>
+//         </div>
+//         <h1 style="color:crimson";>Crimson page??</h1>
+//         </body>
+//     `)
+// })
+
+app.listen(process.env.PORT)
